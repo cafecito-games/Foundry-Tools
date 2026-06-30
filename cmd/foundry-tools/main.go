e44aa1de1eb9686e8ccd548fefd32eed40ffac7e
Christian Sueiras <1381470+csueiras@users.noreply.github.com>
2026-06-30 10:43:46 -0400

    fix: tidy cli skeleton module state

--- cmd/foundry-tools/main.go
+++ cmd/foundry-tools/main.go
@@ -1,3 +1,4 @@
+// Package main provides the foundry-tools command.
 package main
 
 import (
--- cmd/protoc-gen-foundryscript/main.go
+++ cmd/protoc-gen-foundryscript/main.go
@@ -1,3 +1,4 @@
+// Package main provides the protoc-gen-foundryscript plugin command.
 package main
 
 import (
